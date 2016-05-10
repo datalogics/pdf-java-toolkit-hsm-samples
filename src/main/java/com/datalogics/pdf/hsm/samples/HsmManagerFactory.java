0778a755879893352a77cc71ab16f1efa4040f8e
jrowden8 <jrowden@datalogics.com>
2016-05-10 13:56:58 -0500

    HsmManagerFactory: Rename method name.

--- src/main/java/com/datalogics/pdf/hsm/samples/HsmManagerFactory.java
+++ src/main/java/com/datalogics/pdf/hsm/samples/HsmManagerFactory.java
@@ -17,13 +17,13 @@ public final class HsmManagerFactory {
     private HsmManagerFactory() {}
 
     /**
-     * Gets a instance of a HsmManager for the needed type of HSM device.
+     * Gets a new instance of a HsmManager for the needed type of HSM device.
      *
      * @param hsm - Type of HSM device you are connecting to
      * @return HsmManager
      */
 
-    public static HsmManager getInstance(final HsmType hsm) {
+    public static HsmManager newInstance(final HsmType hsm) {
         if (hsm.equals(HsmType.LUNA_SA_HSM)) {
             return new LunaHsmManager();
         }
