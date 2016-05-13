92626096619c9296977e52ccb77623e89c753eb6
jrowden8 <jrowden@datalogics.com>
2016-05-13 17:03:58 -0500

    HsmLoginParms: Add Javadocs.

--- src/main/java/com/datalogics/pdf/security/HsmLoginParms.java
+++ src/main/java/com/datalogics/pdf/security/HsmLoginParms.java
@@ -11,10 +11,20 @@ public class HsmLoginParms {
 
     private final String password;
 
+    /**
+     * Instantiate the login parameters for logging in to any Hsm device using the given password.
+     *
+     * @param pswd - The password to use for the login
+     */
     public HsmLoginParms(final String pswd) {
         this.password = pswd;
     }
 
+    /**
+     * Get the password needed to login into a Hsm device.
+     *
+     * @return password
+     */
     public String getPassword() {
         return password;
     };
