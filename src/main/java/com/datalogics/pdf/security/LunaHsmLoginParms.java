f1bc1a8d8021d555378a00629a797c00ec29bf82
jrowden8 <jrowden@datalogics.com>
2016-05-13 17:04:10 -0500

    LunaHsmLoginParms: Add Javadocs.

--- src/main/java/com/datalogics/pdf/security/LunaHsmLoginParms.java
+++ src/main/java/com/datalogics/pdf/security/LunaHsmLoginParms.java
@@ -11,16 +11,33 @@ public class LunaHsmLoginParms extends HsmLoginParms {
 
     private final String tokenLabel;
 
+    /**
+     * Instantiate the login parameters for logging in to a Luna SA device using the first available Hsm partition slot
+     * and the given password.
+     *
+     * @param pswd - The password to use for the login
+     */
     public LunaHsmLoginParms(final String pswd) {
         super(pswd);
         this.tokenLabel = null;
     }
 
+    /**
+     * Instantiate the login parameters for logging in to a Luna SA Hsm device using the given tokenLabel and password.
+     *
+     * @param tokenLbl - The label of the token to which to login
+     * @param pswd - The password to use for the login
+     */
     public LunaHsmLoginParms(final String tokenLbl, final String pswd) {
         super(pswd);
         this.tokenLabel = tokenLbl;
     }
 
+    /**
+     * Get the Hsm tokenLabel which is synonymous with partition name.
+     *
+     * @return tokenLabel
+     */
     public String getTokenLabel() {
         return tokenLabel;
     }
