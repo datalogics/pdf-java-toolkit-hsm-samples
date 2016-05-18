fae1793c2440cea46e58c240e89b2d045597bcc5
Lyman <robl@dlogics.com>
2016-05-18 11:58:13 -0500

    Have ConfigurationUtils look for properties files in the user's home directory
    This sets up a search strategy for locating properties files.

--- src/main/java/com/datalogics/pdf/hsm/samples/util/ConfigurationUtils.java
+++ src/main/java/com/datalogics/pdf/hsm/samples/util/ConfigurationUtils.java
@@ -5,10 +5,19 @@
 package com.datalogics.pdf.hsm.samples.util;
 
 import org.apache.commons.configuration2.Configuration;
-import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.io.BasePathLocationStrategy;
+import org.apache.commons.configuration2.io.CombinedLocationStrategy;
+import org.apache.commons.configuration2.io.FileLocationStrategy;
+import org.apache.commons.configuration2.io.HomeDirectoryLocationStrategy;
+import org.apache.commons.configuration2.io.ProvidedURLLocationStrategy;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Helper class to provide Apache Configuration objects to clients.
@@ -27,7 +36,32 @@ public final class ConfigurationUtils {
      * @throws ConfigurationException if configuration file could not be found
      */
     public static Configuration getConfiguration(final String configurationFile) throws ConfigurationException {
-        final Configurations configs = new Configurations();
-        return configs.properties(new File(configurationFile));
+        final Parameters parameters = new Parameters();
+        final FileBasedConfigurationBuilder<FileBasedConfiguration> builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                                                                                                                                                                      .configure(parameters.fileBased()
+                                                                                                                                                                                           .setLocationStrategy(buildDefaultLocationStrategy())
+                                                                                                                                                                                           .setFileName(configurationFile));
+        return builder.getConfiguration();
+    }
+
+    /**
+     * Build a location strategy for finding a configuration file.
+     *
+     * <p>
+     * This strategy searches the following locations in order:
+     * <ul>
+     * <li>A provided URL</li>
+     * <li>The current working directory</li>
+     * <li>The user's home directory</li>
+     * </ul>
+     *
+     * @return a strategy for finding a configuration file
+     */
+    private static FileLocationStrategy buildDefaultLocationStrategy() {
+        final List<FileLocationStrategy> strategies = new ArrayList<FileLocationStrategy>();
+        strategies.add(new ProvidedURLLocationStrategy());
+        strategies.add(new BasePathLocationStrategy());
+        strategies.add(new HomeDirectoryLocationStrategy());
+        return new CombinedLocationStrategy(strategies);
     }
 }
