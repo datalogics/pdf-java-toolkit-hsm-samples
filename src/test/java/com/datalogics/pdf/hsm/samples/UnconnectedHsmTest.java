7a1c013ab6dbcaf961c70f422a9b4a2c418302e0
Rob Lyman <robl@datalogics.com>
2016-05-24 11:25:12 -0500

    Refactor UnconnectedHsmTest into individual unit tests
    This now tests three behaviors separately:
    - An unconnected HSM causes an exception to be thrown
    - An unconnected HSM generates a log message
    - An unconnected HSM causes no output to be generated

--- src/test/java/com/datalogics/pdf/hsm/samples/UnconnectedHsmTest.java
+++ src/test/java/com/datalogics/pdf/hsm/samples/UnconnectedHsmTest.java
@@ -11,11 +11,14 @@ import com.datalogics.pdf.hsm.samples.util.LogRecordListCollector;
 import com.datalogics.pdf.security.HsmLoginParameters;
 import com.datalogics.pdf.security.HsmManager;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.security.Key;
 import java.security.cert.Certificate;
@@ -27,48 +30,81 @@ import java.util.logging.Logger;
 /**
  * Unit tests for the HsmSignDocument class.
  */
-public class UnconnectedHsmTest extends SampleTest {
+public class UnconnectedHsmTest {
     static final String FILE_NAME = "SignedField.pdf";
-    static final String QUALIFIED_SIGNATURE_FIELD_NAME = "Approver";
+    static final String MESSAGE = "HsmManager is not connected to HSM device.";
+
+    private static URL inputUrl;
+    private static URL outputUrl;
+    private static File outputFile;
+    private HsmManager unconnectedHsmManager;
 
     @Rule
     public ExpectedException expected = ExpectedException.none();
 
-    @Test
-    public void failWithUnconnectedHsmManager() throws Exception {
-        final URL inputUrl = HsmSignDocument.class.getResource(HsmSignDocument.INPUT_UNSIGNED_PDF_PATH);
-
-        final File file = SampleTest.newOutputFileWithDelete(FILE_NAME);
+    /**
+     * Set up to try and sign a document with an unconnected HSM device.
+     *
+     * @throws IOException an I/O operation failed or was interrupted
+     */
+    @BeforeClass
+    public static void setUp() throws IOException {
+        inputUrl = HsmSignDocument.class.getResource(HsmSignDocument.INPUT_UNSIGNED_PDF_PATH);
+        outputFile = SampleTest.newOutputFileWithDelete(FILE_NAME);
 
         // The complete file name will be set in the HsmSignDocument class.
-        final URL outputUrl = file.toURI().toURL();
+        outputUrl = outputFile.toURI().toURL();
+    }
 
+    /**
+     * Create an unconnected HSM device.
+     */
+    @Before
+    public void createUnconnectedHsm() {
         // Create an unconnected HsmManager
-        final HsmManager unconnectedHsmManager = new MockHsmManager() {
+        unconnectedHsmManager = new MockHsmManager() {
             @Override
             public ConnectionState getConnectionState() {
                 return ConnectionState.READY;
             }
         };
+    }
 
+    @Test
+    public void illegalStateExceptionIsThrown() throws Exception {
         // Expect an IllegalStateException to be thrown
         expected.expect(IllegalStateException.class);
-        expected.expectMessage("HsmManager is not connected to HSM device.");
+        expected.expectMessage(MESSAGE);
+        HsmSignDocument.signExistingSignatureFields(unconnectedHsmManager, inputUrl, outputUrl);
+    }
 
+    @Test
+    public void logMessageIsGenerated() throws Exception {
         final ArrayList<LogRecord> logRecords = new ArrayList<LogRecord>();
         final Logger logger = Logger.getLogger(HsmSignDocument.class.getName());
         try (LogRecordListCollector collector = new LogRecordListCollector(logger, logRecords)) {
             HsmSignDocument.signExistingSignatureFields(unconnectedHsmManager, inputUrl, outputUrl);
+        } catch (final IllegalStateException e) {
+            // Expected exception
         }
 
         // Verify that we got the expected log message
         assertEquals("Must have one log record", 1, logRecords.size());
         final LogRecord logRecord = logRecords.get(0);
-        assertEquals("HsmManager is not connected to HSM device.", logRecord.getMessage());
+        assertEquals(MESSAGE, logRecord.getMessage());
         assertEquals(Level.SEVERE, logRecord.getLevel());
+    }
+
+    @Test
+    public void outputFileIsNotGenerated() throws Exception {
+        try {
+            HsmSignDocument.signExistingSignatureFields(unconnectedHsmManager, inputUrl, outputUrl);
+        } catch (final IllegalStateException e) {
+            // Expected exception
+        }
 
         // Verify the Output file does not exist.
-        assertFalse(file.getPath() + " must not exist after run", file.exists());
+        assertFalse(outputFile.getPath() + " must not exist after run", outputFile.exists());
     }
 
     private class MockHsmManager implements HsmManager {
