36d88e3cb7092d5ba515f633d1d30c1589ec831e
Rob Lyman <robl@datalogics.com>
2016-05-24 10:51:36 -0500

    Verify exception and log record for unconnected HSM test

--- src/test/java/com/datalogics/pdf/hsm/samples/HsmSignDocumentTest.java
+++ src/test/java/com/datalogics/pdf/hsm/samples/HsmSignDocumentTest.java
@@ -4,17 +4,25 @@
 
 package com.datalogics.pdf.hsm.samples;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import com.datalogics.pdf.hsm.samples.util.LogRecordListCollector;
 import com.datalogics.pdf.security.HsmLoginParameters;
 import com.datalogics.pdf.security.HsmManager;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.File;
 import java.net.URL;
 import java.security.Key;
 import java.security.cert.Certificate;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 /**
  * Unit tests for the HsmSignDocument class.
@@ -22,6 +30,9 @@ import java.security.cert.Certificate;
 public class HsmSignDocumentTest extends SampleTest {
     static final String FILE_NAME = "SignedField.pdf";
     static final String QUALIFIED_SIGNATURE_FIELD_NAME = "Approver";
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
 
     @Test
     public void failWithUnconnectedHsmManager() throws Exception {
@@ -40,8 +51,23 @@ public class HsmSignDocumentTest extends SampleTest {
             }
         };
 
-        HsmSignDocument.signExistingSignatureFields(unconnectedHsmManager, inputUrl, outputUrl);
-        // Make sure the Output file does not exist.
+        // Expect an IllegalStateException to be thrown
+        expected.expect(IllegalStateException.class);
+        expected.expectMessage("HsmManager is not connected to HSM device.");
+
+        final ArrayList<LogRecord> logRecords = new ArrayList<LogRecord>();
+        final Logger logger = Logger.getLogger(HsmSignDocument.class.getName());
+        try (LogRecordListCollector collector = new LogRecordListCollector(logger, logRecords)) {
+            HsmSignDocument.signExistingSignatureFields(unconnectedHsmManager, inputUrl, outputUrl);
+        }
+
+        // Verify that we got the expected log message
+        assertEquals("Must have one log record", 1, logRecords.size());
+        final LogRecord logRecord = logRecords.get(0);
+        assertEquals("HsmManager is not connected to HSM device.", logRecord.getMessage());
+        assertEquals(Level.SEVERE, logRecord.getLevel());
+
+        // Verify the Output file does not exist.
         assertFalse(file.getPath() + " must not exist after run", file.exists());
     }
 
