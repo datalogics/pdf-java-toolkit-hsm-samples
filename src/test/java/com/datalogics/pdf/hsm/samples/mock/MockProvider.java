cc242828eee733273468024e4f186e7b02c0df46
Rob Lyman <robl@datalogics.com>
2016-05-26 11:02:02 -0500

    Add a mock security provider and mock signer for testing
    The MockSigner wraps a java.security.Signature object and uses it for
    the work of signing. The MockProvider makes it available to the JCA
    system. This allows us to emulate HSM hardware in software.

---
+++ src/test/java/com/datalogics/pdf/hsm/samples/mock/MockProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Datalogics Inc.
+ */
+
+package com.datalogics.pdf.hsm.samples.mock;
+
+import java.security.Provider;
+
+/**
+ * Mock security provider for testing.
+ */
+public class MockProvider extends Provider {
+
+    public static final String PROVIDER_NAME = "MockProvider";
+
+    /**
+     * Create a MockProvider.
+     */
+    public MockProvider() {
+        this(PROVIDER_NAME, 1.0, "Mock provider for testing.");
+    }
+
+    /**
+     * Constructor for MockProvider.
+     *
+     * @param name provider name
+     * @param version provider version
+     * @param info provider information
+     */
+    protected MockProvider(final String name, final double version, final String info) {
+        super(name, version, info);
+
+        // Provide a MockSigner
+        put("Signature.SHA256withRSA", MockSigner.class.getName());
+    }
+
+    /**
+     * Serial number.
+     */
+    private static final long serialVersionUID = 1L;
+
+}
