3ae3680fbac42429f774b3d9d70db3f82b63abcc
Rob Lyman <robl@datalogics.com>
2016-05-26 11:06:42 -0500

    Modify MockedHsmTest to use MockProvider
    This test now passes, and produces a signed PDF using the mocked HSM
    provider.

--- src/test/java/com/datalogics/pdf/hsm/samples/MockedHsmTest.java
+++ src/test/java/com/datalogics/pdf/hsm/samples/MockedHsmTest.java
@@ -5,13 +5,14 @@
 package com.datalogics.pdf.hsm.samples;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import com.adobe.pdfjt.core.credentials.PrivateKeyHolder;
 import com.adobe.pdfjt.core.credentials.PrivateKeyHolderFactory;
 import com.adobe.pdfjt.core.credentials.impl.ByteArrayKeyHolder;
 import com.adobe.pdfjt.core.credentials.impl.utils.CertUtils;
 
+import com.datalogics.pdf.hsm.samples.mock.MockProvider;
 import com.datalogics.pdf.hsm.samples.util.LogRecordListCollector;
 import com.datalogics.pdf.security.HsmManager;
 
@@ -24,6 +25,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -36,11 +38,11 @@ import java.util.logging.Logger;
 /**
  * Unit tests simulating a connected HSM device.
  */
-public class MockedHsmTest {
+public class MockedHsmTest extends SampleTest {
     private static final Logger LOGGER = Logger.getLogger(MockedHsmTest.class.getName());
 
     static final String FILE_NAME = "SignedField.pdf";
-    static final String MESSAGE = "HsmManager is not connected to HSM device.";
+    static final String MESSAGE = "Fully Qualified Name: Approver";
 
     private static URL inputUrl;
     private static URL outputUrl;
@@ -50,10 +52,10 @@ public class MockedHsmTest {
     /**
      * Set up to try and sign a document with an unconnected HSM device.
      *
-     * @throws IOException an I/O operation failed or was interrupted
+     * @throws Exception a general exception was thrown
      */
     @BeforeClass
-    public static void setUp() throws IOException {
+    public static void setUpForMock() throws Exception {
         inputUrl = HsmSignDocument.class.getResource(HsmSignDocument.INPUT_UNSIGNED_PDF_PATH);
         outputFile = SampleTest.newOutputFileWithDelete(FILE_NAME);
 
@@ -78,19 +80,19 @@ public class MockedHsmTest {
         assertEquals("Must have one log record", 1, logRecords.size());
         final LogRecord logRecord = logRecords.get(0);
         assertEquals(MESSAGE, logRecord.getMessage());
-        assertEquals(Level.SEVERE, logRecord.getLevel());
+        assertEquals(Level.INFO, logRecord.getLevel());
     }
 
     @Test
-    public void outputFileIsNotGenerated() throws Exception {
+    public void outputFileIsGenerated() throws Exception {
         try {
             HsmSignDocument.signExistingSignatureFields(connectedHsmManager, inputUrl, outputUrl);
         } catch (final IllegalStateException e) {
             // Expected exception
         }
 
-        // Verify the Output file does not exist.
-        assertFalse(outputFile.getPath() + " must not exist after run", outputFile.exists());
+        // Verify the Output file exists.
+        assertTrue(outputFile.getPath() + " must exist after run", outputFile.exists());
     }
 
     /*
@@ -103,6 +105,15 @@ public class MockedHsmTest {
         final InputStream certStream = MockedHsmTest.class.getResourceAsStream(DER_CERT_PATH);
         final InputStream keyStream = MockedHsmTest.class.getResourceAsStream(DER_KEY_PATH);
 
+        private Key key;
+        private Certificate[] certificateChain;
+
+        public ConnectedHsmManager() throws Exception {
+            initializeProvider();
+            loadKey();
+            loadCertificateChain();
+        }
+
         @Override
         public ConnectionState getConnectionState() {
             return ConnectionState.CONNECTED;
@@ -110,39 +121,53 @@ public class MockedHsmTest {
 
         @Override
         public Key getKey(final String password, final String keyLabel) {
+            return key;
+        }
+
+        @Override
+        public Certificate[] getCertificateChain(final String certLabel) {
+            return certificateChain;
+        }
+
+        @Override
+        public String getProviderName() {
+            return MockProvider.PROVIDER_NAME;
+        }
+
+        private void initializeProvider() {
+            if (Security.getProvider(MockProvider.PROVIDER_NAME) == null) {
+                Security.addProvider(new MockProvider());
+            }
+        }
+
+        private void loadKey() throws Exception {
             try {
                 final byte[] derEncodedPrivateKey = getDerEncodedData(keyStream);
                 final PrivateKeyHolder privateKeyHolder = PrivateKeyHolderFactory
                                                                                  .newInstance()
                                                                                  .createPrivateKey(derEncodedPrivateKey,
                                                                                                    "RSA");
-                return CertUtils.createJCEPrivateKey(((ByteArrayKeyHolder) privateKeyHolder).getDerEncodedKey(),
-                                                     ((ByteArrayKeyHolder) privateKeyHolder).getAlgorithm());
+                key = CertUtils.createJCEPrivateKey(((ByteArrayKeyHolder) privateKeyHolder).getDerEncodedKey(),
+                                                    ((ByteArrayKeyHolder) privateKeyHolder).getAlgorithm());
             } catch (final IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
                 if (LOGGER.isLoggable(Level.SEVERE)) {
                     LOGGER.severe("Could not create private key: " + e.getMessage());
                 }
+                throw new Exception(e);
             }
-            return null;
         }
 
-        @Override
-        public Certificate[] getCertificateChain(final String certLabel) {
+        private void loadCertificateChain() throws Exception {
             try {
                 final byte[] derEncodedCert = getDerEncodedData(certStream);
                 final X509Certificate jceCert = (X509Certificate) CertUtils.importCertificate(derEncodedCert);
-                return new X509Certificate[] { jceCert };
+                certificateChain = new X509Certificate[] { jceCert };
             } catch (final IOException | CertificateException e) {
                 if (LOGGER.isLoggable(Level.SEVERE)) {
                     LOGGER.severe("Could not create certificate: " + e.getMessage());
                 }
+                throw new Exception(e);
             }
-            return new Certificate[0];
-        }
-
-        @Override
-        public String getProviderName() {
-            return "mock-provider";
         }
 
         private static byte[] getDerEncodedData(final InputStream inputStream) throws IOException {
