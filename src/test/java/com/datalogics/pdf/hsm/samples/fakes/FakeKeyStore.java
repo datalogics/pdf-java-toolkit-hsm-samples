540a80a963caa1bda568e6707fffb68187a62869
Rob Lyman <robl@datalogics.com>
2016-06-03 11:27:35 -0500

    First implementation of certificate generator for FakeKeyStore
    This enables the unit test to pass.

--- src/test/java/com/datalogics/pdf/hsm/samples/fakes/FakeKeyStore.java
+++ src/test/java/com/datalogics/pdf/hsm/samples/fakes/FakeKeyStore.java
@@ -4,9 +4,18 @@
 
 package com.datalogics.pdf.hsm.samples.fakes;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -17,6 +26,7 @@ import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.Enumeration;
 
@@ -25,18 +35,23 @@ import java.util.Enumeration;
  */
 public class FakeKeyStore extends KeyStoreSpi {
     private KeyPair keyPair;
+    private X509Certificate certificate;
 
     /**
      * Constructor for FakeKeyStore.
      *
      * @throws NoSuchProviderException if provider is not found
      * @throws NoSuchAlgorithmException if algorithm is not found
+     * @throws CertificateException if certificate could not be created
+     * @throws OperatorCreationException if certificate could not be created
      */
-    public FakeKeyStore() throws NoSuchAlgorithmException, NoSuchProviderException {
+    public FakeKeyStore() throws NoSuchAlgorithmException, NoSuchProviderException, OperatorCreationException,
+                    CertificateException {
         final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         final SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
         generator.initialize(1024, random);
         keyPair = generator.generateKeyPair();
+        certificate = generateX509Certificate();
     }
 
     /* (non-Javadoc)
@@ -60,7 +75,7 @@ public class FakeKeyStore extends KeyStoreSpi {
      */
     @Override
     public Certificate engineGetCertificate(final String alias) {
-        return null;
+        return certificate;
     }
 
     /* (non-Javadoc)
@@ -161,4 +176,21 @@ public class FakeKeyStore extends KeyStoreSpi {
     @Override
     public void engineLoad(final InputStream stream, final char[] password)
                     throws IOException, NoSuchAlgorithmException, CertificateException {}
+
+    private X509Certificate generateX509Certificate() throws OperatorCreationException, CertificateException {
+        final X500Name issuerName = new X500Name("CN=www.datalogics.com");
+        final X500Name subjectName = issuerName;
+        final BigInteger serial = BigInteger.valueOf(3);
+        final Date notBefore = new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24 * 30);
+        final Date notAfter = new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 30);
+        final X509v3CertificateBuilder x509builder = new JcaX509v3CertificateBuilder(issuerName, serial, notBefore,
+                                                                                 notAfter, subjectName,
+                                                                                 keyPair.getPublic());
+
+        final JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder("SHA1WithRSA");
+        signerBuilder.setProvider("BC");
+        final X509CertificateHolder certHolder = x509builder.build(signerBuilder.build(keyPair.getPrivate()));
+
+        return new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
+    }
 }
